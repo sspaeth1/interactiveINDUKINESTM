e0fe37e7e4ea34ac30fb252beefe6ed4c2c8ec22
sam.spaeth@gmail.com <sam.spaeth@gmail.com>
2020-12-24 14:42:15 -0500

    changes start and end button text to unicode arrows, recolored  yellowCytokine.png and helf-lifeextiensiondomain molecules to brand colors. split out the js file to a seperate file.

---
+++ js/interactive.js
@@ -0,0 +1,104 @@
+const colorScheme = [
+  { cytokineDomain: "yellow" },
+  { halfLifeExtensionDomain: "blue" },
+  { proteaseCleavableLinkers: "red" },
+  { inactivationDomain: "cyan" },
+];
+
+let cytokine = document.querySelector(".cytokine");
+
+(halfLifeExtension = document.querySelector(".cytokinePart.halfLifeExtensionDomain")),
+  (cyanImage = document.querySelector(".cytokinePart.cytokineDomain")),
+  (proteaseCleavableLinkers = document.querySelector(".cytokinePart.proteaseCleavableLinkers")),
+  (inactivationDomain = document.querySelector(".cytokinePart.inactivationDomain")),
+  (buttonPlayReplay = document.querySelector(".begin")),
+  (buttonReset = document.querySelector(".reset"));
+buttonStart = document.querySelector(".start");
+buttonEnd = document.querySelector(".end");
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.defaults({ ease: "circle", duration: 0.5 });
+
+grayScale = () => {
+  gsap.defaults({ ease: "circle", duration: 0.8 });
+
+  let tl = gsap.timeline();
+  tl.from(
+    [
+      halfLifeExtension.firstElementChild,
+      cyanImage.firstElementChild,
+      inactivationDomain.firstElementChild,
+      proteaseCleavableLinkers.firstElementChild,
+    ],
+    { transform: "scale(.7)", y: 50, filter: "grayscale(1)", stagger: 0.2 }
+  );
+  tl.to(
+    [
+      halfLifeExtension.firstElementChild,
+      cyanImage.firstElementChild,
+      inactivationDomain.firstElementChild,
+      proteaseCleavableLinkers.firstElementChild,
+    ],
+    { transform: "scale(1)", filter: "grayscale(0)", stagger: 0.2 }
+  );
+  return tl;
+};
+
+grayScale();
+
+fadeOnMicroEnviro = () => {
+  let tl = gsap.timeline();
+  tl.fromTo(
+    ".membraneAndReceptor",
+    {
+      scrollTrigger: {
+        start: "top bottom",
+        marker: false,
+      },
+      opacity: 0,
+      duration: 2,
+    },
+    { opacity: 1 }
+  );
+};
+
+let tl = gsap.timeline();
+let membraneTrigger = "top bottom";
+tl.addLabel("beginning");
+tl.pause();
+tl.to(".protease", { x: 800, y: -80, duration: 2, opacity: 1, ease: "power1" })
+  .to(".protease", { opacity: 0 })
+  .to(halfLifeExtension, { x: -200, y: 200, opacity: 0, duration: 2 })
+  .to(inactivationDomain, { x: 200, y: 200, opacity: 0, duration: 2 }, "-=2")
+  .to(".proteaseCleavableLinkers", { opacity: 0 }, "-=1")
+  .to(".deliveryText", { opacity: 1 }, "-=1")
+  .to(".cytokine", { y: 150, x: 200, duration: 2, ease: "power1" }, "-=1")
+  .to(".membraneAndReceptor", { opacity: 1, y: -20, duration: 3, "z-index": -2 }, "-=2")
+  .to(".receptor", { y: -30, duration: 3 }, "-=3")
+  .addLabel("end");
+
+buttonPlayReplay.addEventListener("click", () => {
+  if ((buttonPlayReplay.innerText = "play")) {
+    buttonPlayReplay.innerText = "Replay";
+    tl.play("beginning");
+  } else {
+    buttonPlayReplay.innerText = "restart";
+    tl.play("beginning");
+  }
+});
+
+buttonReset.addEventListener("click", () => {
+  tl.seek("beginning");
+  tl.pause();
+  grayScale();
+});
+
+buttonStart.addEventListener("click", () => {
+  tl.seek("beginning");
+  tl.pause();
+  grayScale();
+});
+
+buttonEnd.addEventListener("click", () => {
+  tl.seek("end");
+});
